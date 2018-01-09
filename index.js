8a198f4012629ffd4705ce0e70f84e6c99a9495d
Lindsay Dennis <lindsay.c.dennis@gmail.com>
2018-01-09 23:38:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
   object[key] = value;
   return object;
 }
-function deleteObjectByKey(object, key) {
+function deleteFromObjectByKey(object, key) {
   var newObj = Object.assign({}, object);
   delete newObj.key;
   return newObj;
